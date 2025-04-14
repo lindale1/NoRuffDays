95e024422179474ef9a0b59841e21d03977f6ff4
ugacs-mmadd <168934677+ugacs-mmadd@users.noreply.github.com>
2025-04-13 20:46:52 -0400

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,13 @@
 import AuthHeader from '../../components/AuthHeader';
+import Background from 'Background';
 
 export default function ContactPage() {
   return (
     <div>
       <AuthHeader />
       <main className="p-8">
-       
       </main>
+      <Background />
     </div>
   );
 }
