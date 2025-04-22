5fa0ae6809683e5421c432e1e94cd139ed07b27e
Linda Le <ltl68776@uga.edu>
2025-04-22 10:04:49 -0400

    page not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+export default function NotFound() {
+    return(
+
+        <div>
+            <Header></Header>
+            <h1>404</h1>
+            <p>The page you are trying to connect to cannot be found. Please review the address and try again.</p>
+        </div>
+    );
+}
